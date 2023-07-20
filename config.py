820f11fbc52045dfb08ab8162b779655572cfb27
khaydarthegreat <63006035+khaydarthegreat@users.noreply.github.com>
2023-07-20 11:42:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import database
 
 PAYMENT_MANAGERS = [236030478]
-SALES_MANAGERS = [5709730059,56424449]
+SALES_MANAGERS = [1499768187,56424449]
 ANALYTICS = [56424449]  # Add your analytics group's user IDs here
 
-BOT_TOKEN = "5465597754:AAEwaLJQzakjS2hngumx-On-5DfRpItPHuU"
-MANAGER_URL = "https://t.me/stavki_tochka1"
-BOT_URL = "https://t.me/sitbetbot"
+BOT_TOKEN = "6327221288:AAEuqPM37vR62GLtcwb7bfk8ZAGJMg8HEr4"
+MANAGER_URL = "https://t.me/BRUTAL_S"
+BOT_URL = "https://t.me/brubetbot"
 
 
 
