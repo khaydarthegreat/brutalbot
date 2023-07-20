c1234d4633b254159788131c310741c9ee27eeb5
khaydarthegreat <63006035+khaydarthegreat@users.noreply.github.com>
2023-07-20 11:20:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandle
 from cashier import invoice, handle_payment, go_back, handle_screenshot, approve_invoice, decline_invoice, do_nothing
 from reports import reports, sales_book_report, clients_book_report, input_date, generate_sales_report, generate_clients_report, set_today, set_yesterday, set_this_month, set_this_week,  set_30_days, set_custom_period, START, INPUT_DATE, GENERATE_SALES_BOOK_REPORT, GENERATE_CLIENTS_BOOK_REPORT
 from settings import conv_handler_payments
-from config import PAYMENT_MANAGERS, SALES_MANAGERS, ANALYTICS, get_card_number, set_card_number
+from config import PAYMENT_MANAGERS, SALES_MANAGERS, ANALYTICS, BOT_TOKEN, MANAGER_URL get_card_number, set_card_number
 import re
 import traceback
 import uuid
@@ -100,7 +100,7 @@ def start(update: Update, context: CallbackContext) -> None:
 
         # Creating InlineKeyboardMarkup
         keyboard = [[InlineKeyboardButton("✅ Я оплатил", callback_data='i_paid'),
-                     InlineKeyboardButton("👨‍💼 Менеджер", url='https://t.me/stavki_tochka1')]]  # Replace with the actual username of the sales manager
+                     InlineKeyboardButton("👨‍💼 Менеджер", url=MANAGER_URL)]]  # Replace with the actual username of the sales manager
 
         reply_markup = InlineKeyboardMarkup(keyboard)
 
@@ -149,7 +149,7 @@ def error_callback(update: Update, context: CallbackContext) -> None:
 
 def main() -> None:
     # You should replace 'YOUR BOT TOKEN' with your actual token
-    updater = Updater("5465597754:AAEwaLJQzakjS2hngumx-On-5DfRpItPHuU", use_context=True) 
+    updater = Updater(BOT_TOKEN, use_context=True) 
 
     dispatcher = updater.dispatcher
     dispatcher.add_handler(conv_handler_payments)
