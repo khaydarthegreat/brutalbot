dfae7223eb2070d78f8fa672679453bb2de5bd93
khaydarthegreat <63006035+khaydarthegreat@users.noreply.github.com>
2023-07-20 11:20:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,11 @@ PAYMENT_MANAGERS = [236030478]
 SALES_MANAGERS = [5709730059,56424449]
 ANALYTICS = [56424449]  # Add your analytics group's user IDs here
 
+BOT_TOKEN = "5465597754:AAEwaLJQzakjS2hngumx-On-5DfRpItPHuU"
+MANAGER_URL = "https://t.me/stavki_tochka1"
+
+
+
 
 def get_card_number():
     return database.get_current_card()  # Fetches the current card number from the database
